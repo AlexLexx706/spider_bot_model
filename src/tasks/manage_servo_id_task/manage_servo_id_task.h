d83634f73fa406cf879a872e51b23b05c84243b1
AlexLexx <alexlexx1@gmail.com>
2018-09-12 02:34:06 +0300

    Update manage_servo_id_task.h

--- src/tasks/manage_servo_id_task/manage_servo_id_task.h
+++ src/tasks/manage_servo_id_task/manage_servo_id_task.h
@@ -1,8 +1,23 @@
 #ifndef _MANAGA_SERVO_ID_TASK_H_
 #define _MANAGA_SERVO_ID_TASK_H_
 
+
 class ManagaServoIDTask {
 public:
+	struct InputStore {
+		int cmd;
+		int param;
+	};
+
+	struct OutputStore {
+		int state;
+	};
+
+	struct DataStore{
+		InputStore input;
+		OutputStore input;
+	} store;
+
 	bool init();
 	bool proc();
 };
